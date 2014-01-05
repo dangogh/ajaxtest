720c84086c24abe6ed724b2a81bf00f5da791a53
bayard-webb <bayard.webb@mac.com>
2014-01-04 20:24:42 -0500

    Now reads from html file, ajax not working

--- ajaxtest.go
+++ ajaxtest.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
+	"io"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,40 +15,29 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Server failed to start.")
 	}
-} //main
+}
 
 func timeData(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "I can't tell time yet.")
+	//log.Fatal("I've been hit!")
 }
 
 func HomePage(writer http.ResponseWriter, request *http.Request) {
-	output := `
-<html>
-<body>
-<script type="text/javascript">
- 
-function getTime()
-{
-var ajaxObject ;
-  
-// code for IE7+, Firefox, Chrome, Opera, Safari
-if (window.XMLHttpRequest)
-{
-ajaxObject = new XMLHttpRequest();
-  
-ajaxObject.open(“GET”,”/time”,true);
-ajaxObject.send();
-  
-ajaxObject.onreadystatechange = function()
-{
-if (ajaxObject.readyState == 4 && ajaxObject.status == 200)
-{
-document.getElementById(“time″).innerHTML = "works"// ajaxObject.responseText;
-  
-} } } } </script>
+	inFile, err := os.Open("html/homepage.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	page := ""
+	scanner := bufio.NewScanner(inFile)
+	for scanner.Scan() {
+		page += scanner.Text()
+	}
+	err = scanner.Err()
+	if err == io.EOF {
+		log.Print("EOF Reached")
+	}
 
-<div id="time"> Click to display current time. </div>
-<input id="timeButton" type="button" value="Click" onclick="getTime()" />
-`
-	fmt.Fprint(writer, output)
+	log.Print(page)
+
+	fmt.Fprint(writer, page)
 }
