d4a2c4bf201ad6bdbbaff4b43e0ae9ef7a80071e
bayard-webb <bayard.webb@mac.com>
2014-01-05 14:55:18 -0500

    css file found but is text/plain

--- ajaxtest.go
+++ ajaxtest.go
@@ -7,23 +7,43 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", HomePage)
 	http.HandleFunc("/time", timeData)
+	http.HandleFunc("/base.css", getCSS)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Server failed to start.")
 	}
 }
 
 func timeData(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "I can't tell time yet.")
-	//log.Fatal("I've been hit!")
+	t := time.Now()
+	fmt.Fprint(w, t.String() )
 }
 
 func HomePage(writer http.ResponseWriter, request *http.Request) {
 	inFile, err := os.Open("html/homepage.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	page := ""
+	scanner := bufio.NewScanner(inFile)
+	for scanner.Scan() {
+		page += scanner.Text() + "\n"
+	}
+	err = scanner.Err()
+	if err == io.EOF {
+		log.Print("EOF Reached")
+	}
+
+	fmt.Fprint(writer, page)
+}
+
+func getCSS(writer http.ResponseWriter, request *http.Request) {
+	inFile, err := os.Open("css/base.css")
 	if err != nil {
 		log.Fatal(err)
 	}
